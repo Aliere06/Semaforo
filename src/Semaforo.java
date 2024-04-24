a8c18d9b94446f114672155ab283fd59fbf1eb60
Aliere <allieig666@gmail.com>
2024-04-24 12:59:58 -0600

    GUI Test

--- src/Semaforo.java
+++ src/Semaforo.java
@@ -2,13 +2,30 @@
 /* Clase semáforo que se puede ejecutar en un proceso independiente, la clase tiene como
  * atributo el objeto controlador que se utiliza para sincronizar multiples instacias de
  * esta clase.*/
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Label;
+
 public class Semaforo extends Thread { //Para poderse ejecutar en otro hilo se extiende la clase thread
     private ControladorSemaforos controladorCompartido; // Referencia al controlador compartido
+    private JFrame ventana = new JFrame("Semáforo");
 
     //CONSTRUCTOR
     public Semaforo(ControladorSemaforos controladorCompartido, String nombre) {
         super(nombre);
         this.controladorCompartido = controladorCompartido;
+        JPanel panel = new JPanel(new GridLayout(3, 0));
+        panel.setBackground(Color.DARK_GRAY);
+        panel.add(new Label("1"));
+        panel.add(new Label("2"));
+        panel.add(new Label("3"));
+        ventana.setSize(200, 300);
+        ventana.setContentPane(panel);
+        ventana.setVisible(true);
     }
 
     //M. RUN
